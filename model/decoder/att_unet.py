a6ae0396eb43dbaf956e27030166816566a0b04d
shijun18 <46990068+shijun18@users.noreply.github.com>
2021-12-10 21:31:34 +0800

    add attention unet (non official)

---
+++ model/decoder/att_unet.py
@@ -0,0 +1,210 @@
+import sys 
+sys.path.append('..')
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from model.module import attention as attention
+
+
+
+class Conv2dReLU(nn.Sequential):
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            kernel_size,
+            padding=0,
+            stride=1,
+            use_batchnorm=True,
+            norm_layer=None,
+    ):
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        else:
+            norm_layer = norm_layer
+        
+
+        conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=not (use_batchnorm),
+        )
+
+        if use_batchnorm:
+            bn = norm_layer(out_channels)
+
+        else:
+            bn = nn.Identity()
+
+        relu = nn.ReLU(inplace=True)
+
+        super(Conv2dReLU, self).__init__(conv, bn, relu)
+
+
+
+class Attention_block(nn.Module):
+    def __init__(self,F_g,F_l,F_int):
+        super(Attention_block,self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+            )
+        
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self,g,x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1+x1)
+        psi = self.psi(psi)
+
+        return x*psi
+
+class AttDecoderBlock(nn.Module):
+    def __init__(
+            self,
+            in_channels,
+            skip_channels,
+            out_channels,
+            use_batchnorm=True,
+            norm_layer=None,
+            attention_type=None,
+    ):
+        super().__init__()
+
+        self.reduce = nn.Sequential(
+            nn.Conv2d(in_channels,skip_channels,kernel_size=1,stride=1,padding=0,bias=True),
+		    nn.BatchNorm2d(skip_channels),
+			nn.ReLU(inplace=True)
+        )
+
+        self.attention0 = Attention_block(F_g=skip_channels,F_l=skip_channels,F_int=skip_channels//2)
+
+        self.conv1 = Conv2dReLU(
+            skip_channels + skip_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1,
+            use_batchnorm=use_batchnorm,
+            norm_layer=norm_layer
+        )
+        self.attention1 = attention.Attention(attention_type, in_channels=skip_channels + skip_channels)
+        self.conv2 = Conv2dReLU(
+            out_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1,
+            use_batchnorm=use_batchnorm,
+            norm_layer=norm_layer
+        )
+        self.attention2 = attention.Attention(attention_type, in_channels=out_channels)
+        
+
+    def forward(self, x, skip=None):
+        # print(x.size(),skip.size())
+        x = self.reduce(x)
+        x = F.interpolate(x, scale_factor=2, mode="bilinear",align_corners=False)
+        # print(x.size(),skip.size())
+        skip = self.attention0(g=skip,x=x)
+        if skip is not None:
+            x = torch.cat([x, skip], dim=1)
+            x = self.attention1(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.attention2(x)
+        return x
+
+
+class CenterBlock(nn.Sequential):
+    def __init__(self, in_channels, out_channels, use_batchnorm=True, norm_layer=None):
+        conv1 = Conv2dReLU(
+            in_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1,
+            use_batchnorm=use_batchnorm,
+            norm_layer=norm_layer
+        )
+        conv2 = Conv2dReLU(
+            out_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1,
+            use_batchnorm=use_batchnorm,
+            norm_layer=norm_layer
+        )
+        super().__init__(conv1, conv2)
+
+
+class AttUnetDecoder(nn.Module):
+    def __init__(
+            self,
+            encoder_channels,
+            decoder_channels,
+            n_blocks=4,
+            use_batchnorm=True,
+            norm_layer=None,
+            attention_type=None,
+            center=False,
+    ):
+        super().__init__()
+
+        if n_blocks != len(decoder_channels):
+            raise ValueError(
+                "Model depth is {}, but you provide `decoder_channels` for {} blocks.".format(
+                    n_blocks, len(decoder_channels)
+                )
+            )
+
+        encoder_channels = encoder_channels[::-1]  # reverse channels to start from head of encoder
+
+        # computing blocks input and output channels
+        head_channels = encoder_channels[0]
+        in_channels = [head_channels] + list(decoder_channels[:-1])
+        skip_channels = list(encoder_channels[1:])
+        out_channels = decoder_channels
+
+        if center:
+            self.center = CenterBlock(
+                head_channels, head_channels, use_batchnorm=use_batchnorm,norm_layer=norm_layer
+            )
+        else:
+            self.center = nn.Identity()
+
+        # combine decoder keyword arguments
+        kwargs = dict(use_batchnorm=use_batchnorm, norm_layer=norm_layer, attention_type=attention_type)
+        blocks = [
+            AttDecoderBlock(in_ch, skip_ch, out_ch, **kwargs)
+            for in_ch, skip_ch, out_ch in zip(in_channels, skip_channels, out_channels)
+        ]
+        self.blocks = nn.ModuleList(blocks)
+
+    def forward(self, *features):
+
+        features = features[::-1]  # reverse channels to start from head of encoder
+
+        head = features[0]
+        skips = features[1:]
+
+        x = self.center(head)
+        for i, decoder_block in enumerate(self.blocks):
+            skip = skips[i] if i < len(skips) else None
+            x = decoder_block(x, skip)
+
+        return x
