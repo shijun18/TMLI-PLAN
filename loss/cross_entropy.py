ba8a29ec7400c11f865df944e62a4a9f3281d84d
ustclyd <100670937+ustclyd@users.noreply.github.com>
2022-04-26 15:11:54 +0800

    debug

--- loss/cross_entropy.py
+++ loss/cross_entropy.py
@@ -95,7 +95,7 @@ class OhemCELoss(torch.nn.Module):
         super(OhemCELoss, self).__init__()
         self.thresh = -torch.log(torch.tensor(thresh, requires_grad=False, dtype=torch.float)).cuda()
         self.ignore_lb = ignore_lb
-        self.criteria = nn.CrossEntropyLoss(ignore_index=ignore_lb, reduction='none')
+        self.criteria = torch.nn.CrossEntropyLoss(ignore_index=ignore_lb, reduction='none')
 
     def forward(self, logits, labels):
         if len(labels.size()) > 3:
